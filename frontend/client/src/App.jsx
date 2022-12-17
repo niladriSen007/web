1fe0f76ec5e5defe77ee9d02ba94e8f5ccfd1931
Niladri <niladrisengit12@gmail.com>
2022-12-17 16:30:04 +0530

    initial commit

---
+++ frontend/client/src/App.jsx
@@ -0,0 +1,27 @@
+import { EthProvider } from "./contexts/EthContext";
+import Intro from "./components/Intro/";
+import Setup from "./components/Setup";
+import Demo from "./components/Demo";
+import Footer from "./components/Footer";
+import "./App.css";
+
+function App() {
+  return (
+    <EthProvider>
+      Hi
+      <div id="App" >
+        <div className="container">
+          <Intro />
+          <hr />
+          <Setup />
+          <hr />
+          <Demo />
+          <hr />
+          <Footer />
+        </div>
+      </div>
+    </EthProvider>
+  );
+}
+
+export default App;
